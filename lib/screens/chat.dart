43bbb4e632a1610e8d12a62d8daaa0125c1ae188
Raghav-995 <raghavendrapathak0@gmail.com>
2024-04-23 13:30:26 +0530

    adding firebase notification

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,28 @@
 import 'package:chatapp/widgets/chat_messages.dart';
 import 'package:chatapp/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void pushNotifications() async {
+    final notify = FirebaseMessaging.instance;
+    await notify.requestPermission();
+    notify.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
